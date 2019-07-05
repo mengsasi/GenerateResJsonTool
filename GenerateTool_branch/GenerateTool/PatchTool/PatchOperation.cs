af2cedd5cf07b7d04269a84e09cb7d8af5c0015f
mengsasi <mengsasi@outlook.com>
2019-07-05 17:12:02 +0800

    patch

---
+++ GenerateTool_branch/GenerateTool/PatchTool/PatchOperation.cs
@@ -0,0 +1,152 @@
+﻿using Force.Crc32;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace GenerateTool.PatchTool {
+
+    class PatchOperation {
+
+        public static void MainPatch(JToken configJToken) {
+
+            var initPath = configJToken["initPath"].Value<string>();
+            var initVersionPath = configJToken["initVersionPath"].Value<string>();
+            var patchPath = configJToken["patchPath"].Value<string>();
+            var patchVersionPath = configJToken["patchVersionPath"].Value<string>();
+
+            var rootPath = Program.ProjectRootPath;
+
+
+            #region 生成初始版本
+
+            var initDirPath = rootPath + "\\" + initPath;//原版本resource文件夹路径
+            var initVersionFilePath = rootPath + "\\" + initVersionPath;//生成的 init.txt 路径
+
+            using(TextWriter writer = new StreamWriter(initVersionFilePath)) {
+                var roots = Directory.GetDirectories(initDirPath);
+                foreach(var root in roots) {
+                    var files = Directory.GetFiles(root, "*.*", SearchOption.AllDirectories);
+                    foreach(var file in files) {
+                        FileStream stream = new FileStream(file, FileMode.Open);
+                        byte[] buffer = new byte[stream.Length];
+                        stream.Read(buffer, 0, (int)stream.Length);
+                        stream.Close();
+                        uint crc32 = Crc32Algorithm.Compute(buffer);
+                        Console.WriteLine("处理文件：{0}-{1}", Path.GetFileName(file), crc32);
+
+                        var fileRelatePath = file.Substring(initDirPath.Length + 1);
+                        fileRelatePath = fileRelatePath.Replace('\\', '/');
+                        writer.WriteLine("{0}:{1}", fileRelatePath, crc32);
+                    }
+                }
+            }
+
+            #endregion
+
+
+            #region 生成patch文件
+
+            //删除之前创建的
+            string gangsterRes000 = "gangsterRes";// Program.VersionFolder;
+            var lastResDir = rootPath + "\\" + gangsterRes000;
+            if(Directory.Exists(lastResDir)) {
+                Directory.Delete(lastResDir, true);
+            }
+
+            var patchDirPath = rootPath + "\\" + patchPath;//新版本resource文件夹路径
+            var patchVersionFilePath = rootPath + "\\" + patchVersionPath;//生成的 1.2.0.txt 路径
+
+            Dictionary<string, uint> initFileCrc = new Dictionary<string, uint>();
+            using(TextReader reader = new StreamReader(initVersionFilePath)) {
+                while(true) {
+                    var line = reader.ReadLine();
+                    if(string.IsNullOrEmpty(line)) {
+                        break;
+                    }
+
+                    var parts = line.Split(':');
+                    var fileRelatePath = parts[0];
+                    uint crc32 = uint.Parse(parts[1]);
+
+                    initFileCrc.Add(fileRelatePath, crc32);
+                }
+            }
+
+            using(TextWriter patchWriter = new StreamWriter(patchVersionFilePath)) {
+                var roots = Directory.GetDirectories(patchDirPath);
+                foreach(var root in roots) {
+                    var files = Directory.GetFiles(root, "*.*", SearchOption.AllDirectories);
+                    foreach(var file in files) {
+                        FileStream stream = new FileStream(file, FileMode.Open);
+                        byte[] buffer = new byte[stream.Length];
+                        stream.Read(buffer, 0, (int)stream.Length);
+                        stream.Close();
+
+                        uint crc32 = Crc32Algorithm.Compute(buffer);
+                        var fileRelatePath = file.Substring(patchDirPath.Length + 1);
+                        fileRelatePath = fileRelatePath.Replace('\\', '/');
+                        if(initFileCrc.ContainsKey(fileRelatePath)) {
+                            if(initFileCrc[fileRelatePath] == crc32) {
+                                continue;
+                            }
+                        }
+                        Console.WriteLine("新增Patch文件：{0}-{1}", Path.GetFileName(file), crc32);
+                        patchWriter.WriteLine("{0}:{1}", fileRelatePath, crc32);
+
+                        if(file.Contains("exml") || file.Contains("Web") || file.Contains("web")) {
+                            continue;
+                        }
+
+                        //复制修改的文件到指定文件夹
+                        PatchOperation.CopyToNewPath(file, fileRelatePath);
+
+                    }
+                }
+            }
+            Console.WriteLine("生成Patch文件成功");
+
+            #endregion
+
+        }
+
+        private static void CopyToNewPath(string sourcePath, string targetRelate) {
+            string root = Program.ProjectRootPath;
+            string gangsterRes000 = "gangsterRes";// Program.VersionFolder;
+            var targetPath = root + "\\" + gangsterRes000 + "\\" + targetRelate;
+            PatchOperation.MakeDir(targetPath);
+            File.Copy(sourcePath, targetPath, true);
+        }
+
+        private static void MakeDir(string filePath) {
+            var root = Program.ProjectRootPath.Replace('\\', '/');
+            string path = filePath.Replace('\\', '/');
+            var fileRelatePath = PatchOperation.GetRelatePath(filePath, root);
+
+            var dirs = fileRelatePath.Split('/');
+            var dirPath = root;
+            for(int i = 0; i < dirs.Length - 1; i++) {
+                string dir = dirs[i];
+                if(dir == "") {
+                    if(!Directory.Exists(dirPath)) {
+                        Directory.CreateDirectory(dirPath);
+                    }
+                }
+                else {
+                    dirPath = dirPath + "/" + dir;
+                    if(!Directory.Exists(dirPath)) {
+                        Directory.CreateDirectory(dirPath);
+                    }
+                }
+            }
+        }
+
+        public static string GetRelatePath(string filePath, string rootPath) {
+            var fileRelatePath = filePath.Substring(rootPath.Length);
+            fileRelatePath = fileRelatePath.Replace('\\', '/');
+            return fileRelatePath;
+        }
+
+
+    }
+}
--- GenerateTool_branch/GenerateTool/Program.cs
+++ GenerateTool_branch/GenerateTool/Program.cs
@@ -20,20 +20,20 @@ namespace GenerateTool {
 
         public static bool AddVersion = false;
 
-        static void Main( string[] args ) {
+        static void Main(string[] args) {
 
-            Console.WriteLine( "请稍候，不要关闭程序..." );
+            Console.WriteLine("请稍候，不要关闭程序...");
 
 
             #region 读取配置
 
             string curFolder = Environment.CurrentDirectory;
-            var configJsonText = FileUtil.Load( curFolder + "\\config.json" );
-            JToken configJToken = JToken.Parse( configJsonText );
+            var configJsonText = FileUtil.Load(curFolder + "\\config.json");
+            JToken configJToken = JToken.Parse(configJsonText);
 
-            DirectoryInfo curDirInfo = new DirectoryInfo( curFolder );
+            DirectoryInfo curDirInfo = new DirectoryInfo(curFolder);
             string root = curDirInfo.Parent.FullName;//工程目录
-            if( configJToken["root"] != null ) {
+            if(configJToken["root"] != null) {
                 root = configJToken["root"].Value<string>();//配置的root路径
             }
             Program.ProjectRootPath = root;
@@ -43,30 +43,30 @@ namespace GenerateTool {
             Program.ResourcePath = resourcePath;
 
             string webResourcePath = root;
-            if( configJToken["webResourcePath"] != null ) {
+            if(configJToken["webResourcePath"] != null) {
                 webResourcePath += configJToken["webResourcePath"].Value<string>();
                 webResourcePath += "\\";
             }
             Program.WebResourcePath = webResourcePath;
             //是否忽略本地中的WebAvatar文件夹
-            if( configJToken["ignoreWeb"] != null ) {
+            if(configJToken["ignoreWeb"] != null) {
                 Program.IgnoreWeb = configJToken["ignoreWeb"].Value<bool>();
             }
             string version = "";
-            if( configJToken["version"] != null ) {
+            if(configJToken["version"] != null) {
                 version = configJToken["version"].Value<string>();
             }
             Program.Version = version;//版本号
             string confVersionFolder = configJToken["versionFolder"].Value<string>();//版本文件夹名gangsterRes
-            Program.VersionFolder = confVersionFolder + Program.Version.Replace( ".", "" );//gangsterRes001
+            Program.VersionFolder = confVersionFolder + Program.Version.Replace(".", "");//gangsterRes001
 
-            if( configJToken["generate"] != null ) {
+            if(configJToken["generate"] != null) {
                 AssetOperation.isGenerate = configJToken["generate"].Value<bool>(); ;
             }
-            if( configJToken["uploading"] != null ) {
+            if(configJToken["uploading"] != null) {
                 AssetOperation.isUploading = configJToken["uploading"].Value<bool>();
             }
-            if( configJToken["uploadingOuterNet"] != null ) {
+            if(configJToken["uploadingOuterNet"] != null) {
                 AssetOperation.isUploadingOuterNet = configJToken["uploadingOuterNet"].Value<bool>();
             }
 
@@ -87,17 +87,17 @@ namespace GenerateTool {
             Program.IgnoreWeb = true;
             //先在Tools文件夹生成一个
             FolderGenerate.Init();
-            if( generateLocal ) {
+            if(generateLocal) {
                 string tooldefaultResJson = curFolder + "\\" + configJToken["defaultresjson"].Value<string>();
-                GenerateUtil.GenerateLocal( configJToken, resourcePath, tooldefaultResJson );
+                GenerateUtil.GenerateLocal(configJToken, resourcePath, tooldefaultResJson);
             }
 
             Program.IgnoreWeb = tempIgnoreWeb;
             //生成本地使用的
             FolderGenerate.Init();
-            if( generateLocal ) {
+            if(generateLocal) {
                 string defaultResJson = resourcePath + configJToken["defaultresjson"].Value<string>();
-                GenerateUtil.GenerateLocal( configJToken, resourcePath, defaultResJson );
+                GenerateUtil.GenerateLocal(configJToken, resourcePath, defaultResJson);
             }
 
             #endregion
@@ -105,13 +105,26 @@ namespace GenerateTool {
 
             #region 更改的资源
 
-            Program.IgnoreWeb = false;//不忽略，就是生成网络资源
-            Program.AddVersion = true;
-            VersionUtils.UseVersionSuffix = true;//加版本号后缀
-            FolderGenerate.Init();
+            //Program.IgnoreWeb = true;//不忽略，就是生成网络资源
+            //Program.AddVersion = true;
+            //VersionUtils.UseVersionSuffix = true;//加版本号后缀
+            //FolderGenerate.Init();
 
-            if( AssetOperation.isGenerate ) {
-                AssetOperation.MainOperation( root, configJToken );
+            //if( AssetOperation.isGenerate ) {
+            //    AssetOperation.MainOperation( root, configJToken );
+            //}
+
+            #endregion
+
+
+            #region 生成patch，将更改的文件复制到根目录一个文件夹
+
+            //是否生成patch更改的资源文件夹
+            var IsGeneratePathDir = configJToken["generatePathDir"].Value<bool>();
+            if(IsGeneratePathDir) {
+
+                PatchTool.PatchOperation.MainPatch(configJToken);
+
             }
 
             #endregion 
@@ -120,11 +133,11 @@ namespace GenerateTool {
             #region 生成网络res.json
 
             var generateWeb = configJToken["generateWeb"].Value<bool>();
-            if( generateWeb ) {
+            if(generateWeb) {
                 string resName = configJToken["gangsterresjson"].Value<string>();
-                resName = resName.Replace( "[version]", Program.Version );
+                resName = resName.Replace("[version]", Program.Version);
                 string gangsterResJson = webResourcePath + resName;
-                GenerateUtil.GenerateWeb( configJToken, webResourcePath, gangsterResJson );
+                GenerateUtil.GenerateWeb(configJToken, webResourcePath, gangsterResJson);
                 Program.Gangster_0_0_0_Path = gangsterResJson;
                 Program.Gangster0_0_0_res_json = resName;
             }
@@ -134,29 +147,29 @@ namespace GenerateTool {
 
             #region 上传ftp
 
-            FTPUtils.InitFTP( configJToken );
+            FTPUtils.InitFTP(configJToken);
             MainUploading.MainUpload();
 
             #endregion 
 
 
-            if( needStop ) {
+            if(needStop) {
                 Console.ReadKey();
             }
         }
 
         //遍历时，web开头文件夹忽略continue
-        public static bool CheckWebFolder( string path ) {
-            if( Program.IgnoreWeb ) {
-                if( path.StartsWith( "web" ) || path.StartsWith( "Web" ) ) {
+        public static bool CheckWebFolder(string path) {
+            if(Program.IgnoreWeb) {
+                if(path.StartsWith("web") || path.StartsWith("Web")) {
                     return true;
                 }
             }
             return false;
         }
 
-        public static void ConsoleLog( string log ) {
-            Console.WriteLine( log );
+        public static void ConsoleLog(string log) {
+            Console.WriteLine(log);
             Program.needStop = true;
         }
 
